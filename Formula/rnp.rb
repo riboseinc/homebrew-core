5e2b9bb2a4bb0d493cd1110c39d666613b049686
Ronald Tse <ronald.tse@ribose.com>
2017-03-23 17:20:26 +0800

    rnp 3.99.18 (new formula)
    Updates formula according to pull request #11504

--- Formula/rnp.rb
+++ Formula/rnp.rb
@@ -3,7 +3,7 @@ class Rnp < Formula
   homepage "https://github.com/riboseinc/rnp"
   url "https://github.com/riboseinc/rnp/archive/3.99.18.tar.gz"
   sha256 "b61ae76934d4d125660530bf700478b8e4b1bb40e75a4d60efdb549ec864c506"
-  head "https://github.com/riboseinc/rnp.git", :using => :git
+  head "https://github.com/riboseinc/rnp.git"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
@@ -11,26 +11,19 @@ class Rnp < Formula
   depends_on "openssl"
 
   def install
-    # Generate the configure/Make files. Ideally this won't be necessary
-    # in the future.
 
-    mkdir "m4"
+    (buildpath/"m4").mkpath
     system "autoreconf", "-ivf"
-
-    # Configure, make, and install.
 
     openssl = Formula["openssl"]
 
     ENV.append "CFLAGS", "-I#{openssl.opt_include}"
     ENV.append "LDFLAGS", "-L#{openssl.opt_lib}"
 
-    args = %W[
-      --prefix=#{prefix}
-      --mandir=#{man}
-      --with-openssl=#{openssl.opt_prefix}
-    ]
+    system "./configure", "--prefix=#{prefix}",
+                          "--mandir=#{man}",
+                          "--with-openssl=#{openssl.opt_prefix}"
 
-    system "./configure", *args
     system "make", "install"
   end
 
