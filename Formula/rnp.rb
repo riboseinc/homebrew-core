df9e3f02000e0a25fff908f837bd4f404c0e9ad2
Ronald Tse <ronald.tse@ribose.com>
2017-03-23 16:28:31 +0800

    rnp 3.99.18 (new formula)
    This formula installs the latest rnp (https://github.com/riboseinc/rnp),
    a fork of the NetPGP package from NetBSD that runs on macOS and Linux.

---
+++ Formula/rnp.rb
@@ -0,0 +1,40 @@
+class Rnp < Formula
+  desc "The Ribose fork of NetPGP."
+  homepage "https://github.com/riboseinc/rnp"
+  url "https://github.com/riboseinc/rnp/archive/3.99.18.tar.gz"
+  sha256 "b61ae76934d4d125660530bf700478b8e4b1bb40e75a4d60efdb549ec864c506"
+  head "https://github.com/riboseinc/rnp.git", :using => :git
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "openssl"
+
+  def install
+    # Generate the configure/Make files. Ideally this won't be necessary
+    # in the future.
+
+    mkdir "m4"
+    system "autoreconf", "-ivf"
+
+    # Configure, make, and install.
+
+    openssl = Formula["openssl"]
+
+    ENV["CFLAGS"]  = "-I#{openssl.opt_include}"
+    ENV["LDFLAGS"] = "-L#{openssl.opt_lib}"
+
+    args = %W[
+      --prefix=#{prefix}
+      --mandir=#{man}
+      --with-openssl=#{openssl.opt_prefix}
+    ]
+
+    system "./configure", *args
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/netpgp", "--version"
+  end
+end
